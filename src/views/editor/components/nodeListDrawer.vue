3995d00e121b7e3d59a4ab30d18a1b727fbb861c
little bowl <littlebowlnju@163.com>
2021-04-06 16:42:15 +0800

    feat: add nodeListDrawer

---
+++ src/views/editor/components/nodeListDrawer.vue
@@ -0,0 +1,38 @@
+<template>
+    <el-drawer
+            :with-header="false"
+            :visible.sync="nodeListVisible"
+            size="20%"
+            :before-close="handleClose"
+    >
+        <el-tabs stretch="true" type="card">
+            <el-tab-pane label="节点列表"></el-tab-pane>
+            <el-tab-pane label="关系列表"></el-tab-pane>
+        </el-tabs>
+    </el-drawer>
+</template>
+
+<script>
+    import {mapGetters, mapMutations} from "vuex";
+
+    export default {
+        name: "nodeListDrawer",
+        computed:{
+            ...mapGetters([
+                'nodeListVisible',
+            ])
+        },
+        methods:{
+            ...mapMutations([
+                'set_nodeListVisible'
+            ]),
+            handleClose(){
+                this.set_nodeListVisible(false);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
