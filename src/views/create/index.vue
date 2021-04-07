c2fb7f8d43380c4c5435eff39f4175b6f69bcf67
wb <2377651630@qq.com>
2021-04-07 16:54:36 +0800

    refactor: jenkins Run

--- src/views/create/index.vue
+++ src/views/create/index.vue
@@ -10,7 +10,7 @@
                 csv文件格式：节点-节点-关系 三元组
                 <el-upload
                         drag
-                        action="http://localhost:8002/coinservice/file/getCsv"
+                        action="http://106.15.93.81:7474/browser/:8002/coinservice/file/getCsv"
                         class="uploading"
                         on-success="handleCsvSuccess"
                         data="uploadParam"
