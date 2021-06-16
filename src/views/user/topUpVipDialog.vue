2ffe1ba8c4b91b24544fb21bc273df9af4c0b243
little bowl <littlebowlnju@163.com>
2021-06-16 20:00:35 +0800

    feat: 我的会员展示

---
+++ src/views/user/topUpVipDialog.vue
@@ -0,0 +1,20 @@
+<template>
+    <div
+            title="会员充值"
+            :visible="topUpVipDialogVisible"
+            :before-close="cancelTopUp"
+    >
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "topUpVipDialog"
+    }
+</script>
+
+<style scoped>
+
+</style>
