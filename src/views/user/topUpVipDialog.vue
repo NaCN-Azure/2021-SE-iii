bdfc50e2098c7d056f81193e5ec1de8d17b33fea
little bowl <littlebowlnju@163.com>
2021-06-16 20:53:36 +0800

    feat: 会员over

--- src/views/user/topUpVipDialog.vue
+++ src/views/user/topUpVipDialog.vue
@@ -6,19 +6,19 @@
     >
         <el-row :gutter="9" style="margin-bottom: 20px">
             <el-col :span="8">
-                <el-card shadow="hover" class="top-up-choice">
+                <el-card shadow="hover" class="top-up-choice" @click.native="setDays(30)">
                     <div class="time">一个月</div>
                     <div class="money">￥15</div>
                 </el-card>
             </el-col>
             <el-col :span="8">
-                <el-card shadow="hover" class="top-up-choice">
+                <el-card shadow="hover" class="top-up-choice" @click.native="setDays(180)">
                     <div class="time">6个月</div>
                     <div class="money">￥80</div>
                 </el-card>
             </el-col>
             <el-col :span="8">
-                <el-card shadow="hover" class="top-up-choice">
+                <el-card shadow="hover" class="top-up-choice" @click.native="setDays(365)">
                     <div class="time">一年</div>
                     <div class="money">￥150</div>
                 </el-card>
@@ -38,30 +38,54 @@
 </template>
 
 <script>
-    import {mapGetters, mapMutations} from "vuex";
+    import {mapActions, mapGetters, mapMutations} from "vuex";
+    import {setVipUserAPI} from "../../api/users";
+    import {Message} from "element-ui";
 
     export default {
         name: "topUpVipDialog",
         data(){
             return{
-
+                days:''
             }
         },
         computed:{
             ...mapGetters([
-                'topUpVipDialogVisible'
+                'topUpVipDialogVisible',
+                'userInfo'
             ])
         },
         methods:{
             ...mapMutations([
                 'set_topUpVipDialogVisible'
             ]),
+            ...mapActions([
+                'getUserInfo'
+            ]),
+            setDays(days){
+                this.days = days;
+            },
             cancelTopUp(){
                 this.set_topUpVipDialogVisible(false);
 
             },
             submitTopUp(){
+                setVipUserAPI(this.userInfo.id,this.days).then(res => {
+                    console.log(res)
+                    if(res.data.code == 200){
+                        Message({
+                            type:'success',
+                            message:'充值成功'
+                        })
+                    }else{
+                        Message({
+                            type:'error',
+                            message:'充值失败'
+                        })
+                    }
+                })
                 this.set_topUpVipDialogVisible(false);
+                this.getUserInfo()
             }
         }
     }
@@ -79,5 +103,8 @@
     }
     .money{
         font-size: 15px;
+    }
+    .el-card{
+        cursor: pointer;
     }
 </style>
